2cae5ea681b9c62f395a51b62ef955839908071b
Lirae Que Data <liraedata59@gmail.com>
2023-07-04 09:40:55 +0800

    Added user and contact models

--- App/models/contact_model.py
+++ App/models/contact_model.py
@@ -3,7 +3,41 @@ from marshmallow import fields
 
 
 class ContactModel(DB.Model):
-    pass
+    __tablename__ = "contact"
+    id = DB.Column(DB.Integer, primary_key = True)
+    user_id = DB.Column(DB.Integer, ) 
+
+    first_name = DB.Column(DB.String(40))    
+    last_name = DB.Column(DB.String(40))    
+    email = DB.Column(DB.String(150), unique=True)
+    phone_no = DB.Column(DB.String(20), unique=True)
+    telephone_no = DB.Column(DB.String(20), unique=True)
+
+    date = DB.Column(DB.Date(timezone=True), default=DB.func.current_date())
+    
+
+    def __init__(self, first_name : str, last_name :str, email : str,
+                phone_no : str, telephone_no : str):
+        self.first_name = first_name 
+        self.last_name = last_name 
+        self.email = email 
+        self.phone_no = phone_no 
+        self.telephone_no = telephone_no 
+
+
+    def __repr__(self) -> str:
+        return "<Contact %r>"%(self.first_name + " " + self.last_name)
+    
+
+    def toObject(self) -> dict:
+        return {
+            "id" : self.id,
+            "first_name" : self.first_name,
+            "last_name" : self.last_name,
+            "email" : self.email,
+            "phone_no" : self.phone_no,
+            "telephone_no" : self.telephone_no
+        }
 
 
 
@@ -11,7 +45,21 @@ class ContactSchema(MALLOW.Schema):
     
     class Meta:
         model : ContactModel = ContactModel
-        fields : tuple = ()
+        fields : tuple = (
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "phone_no",
+            "telephone_no"
+        )
+
+    id = fields.Integer()
+    first_name = fields.String()
+    last_name = fields.String()
+    email = fields.Email()
+    phone_no = fields.String()
+    telephone_no = fields.String()
 
 
 contact_schema : ContactSchema = ContactSchema()
--- App/models/user_model.py
+++ App/models/user_model.py
@@ -3,15 +3,59 @@ from marshmallow import fields
 
 
 class UserModel(DB.Model):
-    pass
+    __tablename__ = "user"
+    id = DB.Column(DB.Integer, primary_key=True)
+    contact = DB.relationship("ContactModel", DB.ForeignKey("user.id"))
+
+    name = DB.Column(DB.String(100), nullable=False)
+    email = DB.Column(DB.String(150), nullable=False, unique=True)
+    password = DB.Column(DB.String(150), nullable=False)
+    phone_no = DB.Column(DB.String(20))
+    telephone_no = DB.Column(DB.String(20))
+    no_of_contact = DB.Column(DB.Integer, default=0)
 
 
+    def __init__(self, name : str, email : str, password : str, phone_no : str):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.phone_no = phone_no
 
+
+    def __repr__(self) -> str:
+        return "<User %r>"%self.name
+    
+
+    def toObject(self) -> dict:
+        return {
+            "id" : self.id,
+            "name" : self.name,
+            "email" : self.email,   
+            "phone_no" : self.phone_no,
+            "telephone_no" : self.telephone_no,
+            "no_of_contact" : self.no_of_contact
+        }
+    
+    
 class UserSchema(MALLOW.Schema):
     
     class Meta:
         model : UserModel = UserModel
-        fields : tuple = ()
+        fields : tuple = (
+            "id", 
+            "name", 
+            "email", 
+            "phone_no", 
+            "telephone_no", 
+            "no_of_contact"
+        )
+
+    id = fields.Integer()
+    name = fields.String()
+    email = fields.Email()
+    phone_no = fields.String()
+    telephone_no = fields.String()
+    no_of_contact = fields.Integer()
 
 
 
